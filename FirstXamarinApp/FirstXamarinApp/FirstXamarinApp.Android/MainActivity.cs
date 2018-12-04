f30289f5405d1af7b1380ef5fe7ada93ea6ffc64
Bhupal <bhupalgoud@gmail.com>
2018-12-04 12:37:43 +0530

    Added packages "Microsoft.AppCenter.Crashes" and "Microsoft.AppCenter.Analytics"

--- FirstXamarinApp/FirstXamarinApp/FirstXamarinApp.Android/MainActivity.cs
+++ FirstXamarinApp/FirstXamarinApp/FirstXamarinApp.Android/MainActivity.cs
@@ -6,6 +6,9 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace FirstXamarinApp.Droid
 {
@@ -14,6 +17,8 @@ namespace FirstXamarinApp.Droid
     {
         protected override void OnCreate(Bundle bundle)
         {
+            AppCenter.Start("cca90fc0-ced8-48e8-bfe6-bfcab42fdc58", typeof(Analytics), typeof(Crashes));
+
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
